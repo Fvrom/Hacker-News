0ae1d9aebfbdd0c5a0511bd846ea62433db6a868
Ida <Fvrom@users.noreply.github.com>
2020-12-16 15:15:05 +0100

    login system added

--- app/functions.php
+++ app/functions.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 // function for redirecting pages
 function redirect(string $path)
 {
-    header("Location: ${path}");
+    header("Location: /index.php");
     exit;
 }
--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php require __DIR__ . '/app/autoload.php'; ?>
 <?php require __DIR__ . '/header.php'; ?>
 
-<p>Home page</p>
+<section>
+    <p>Home page</p>
+
+    <?php if (isset($_SESSION['user'])) : ?>
+        <p> You are logged in, <?= $_SESSION['user']['username']; ?> ! </p>
+
+    <?php endif; ?>
+
+</section>
 
 <?php require __DIR__ . '/footer.php'; ?>
