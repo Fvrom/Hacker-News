0005ca32f376c5f29274d34e96657b05aec6cb65
root <root@Ida-Dator.localdomain>
2021-01-06 14:05:42 +0100

    User can now edit and delete comments

--- header.php
+++ header.php
@@ -4,8 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-
     <link rel="stylesheet" href="/style/global.css">
     <link rel="stylesheet" href="/style/navigation.css">
     <link rel="stylesheet" href="/style/signup.css">
@@ -13,14 +11,11 @@
     <link rel="stylesheet" href="/style/home-page.css">
     <link rel="stylesheet" href="/style/profile.css">
     <link rel="stylesheet" href="/style/settings.css">
-
     <title>Hacker news</title>
 </head>
 
 <body>
     <header>
-        <h1 class="title"> Hacker News</h1>
-        <?php
-        require __DIR__ . '/navigation.php';
-        ?>
+        <h1 class="title">Hacker News</h1>
+        <?php require __DIR__ . '/navigation.php'; ?>
     </header>
